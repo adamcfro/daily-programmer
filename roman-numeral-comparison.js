8d355da8eaa587c14b55b91dedf0ae9a7b9ac715
Adam Froehlich <adamcfro@gmail.com>
2022-03-12 11:03:23 -0500

    Added documentation to roman-numeral-comparison solution

--- roman-numeral-comparison.js
+++ roman-numeral-comparison.js
@@ -1,17 +1,26 @@
-
+/**
+ * Computes the Roman Numerals from two strings using only additive notion 
+ * (not subtractive notion), and returns true if the sum value of the elements 
+ * in the first string parameter is less than the sum of the values 
+ * of the second parameter.
+ * 
+ * @param {String} str1 - First string of Roman Numerals
+ * @param {String} str2 - Second string of Roman Numerals
+ * @returns {Boolean} - Returns a Boolean
+ */
 function romanNumeralComparison (str1, str2) {
   let values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1};
-  let count1 = 0;
-  let count2 = 0;
+  let total1 = 0;
+  let total2 = 0;
   let splitString1 = str1.split("");
   let splitString2 = str2.split("");
   for (let i = 0; i < splitString1.length; i++) {
     let key = splitString1[i];
-    count1 += values[key];
+    total1 += values[key];
   }
   for (let i = 0; i < splitString2.length; i++) {
     let key = splitString2[i];
-    count2 += values[key];
+    total2 += values[key];
   }
-  return count1 < count2;
+  return total1 < total2;
 }
