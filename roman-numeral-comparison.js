8e461d10dae784d39207fdcf9a3a25390aaa354f
Adam Froehlich <adamcfro@gmail.com>
2022-03-13 10:10:39 -0400

    Changed variable name for easier readability

--- roman-numeral-comparison.js
+++ roman-numeral-comparison.js
@@ -9,19 +9,19 @@
  * @returns {Boolean} - Returns a Boolean
  */
 function romanNumeralComparison (str1, str2) {
-  let values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1};
+  let numeralValues = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1};
   let total = 0;
   let splitString1 = str1.split("");
   let splitString2 = str2.split("");
   let maxLength = str1.length >= str2.length ? str1.length : str2.length;
   for (let i = 0, j = 0; i < maxLength && j < maxLength; i++, j++) {
     if (i < str1.length && j < str2.length) {
-      total += values[splitString1[i]];
-      total -= values[splitString2[j]];
+      total += numeralValues[splitString1[i]];
+      total -= numeralValues[splitString2[j]];
     } else if (i < maxLength) {
-      total += values[splitString1[i]];
+      total += numeralValues[splitString1[i]];
     } else {
-      total -= values[splitString2[j]];
+      total -= numeralValues[splitString2[j]];
     }
   }
   return total < 0;
