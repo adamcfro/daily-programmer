19a13514a383ef631d5d716e933f7864aa9c790e
Adam Froehlich <adamcfro@gmail.com>
2022-05-31 09:01:01 -0400

    Added description for Kaprekars Routine

--- kaprekars-routine.js
+++ kaprekars-routine.js
@@ -2,6 +2,12 @@
  * This function takes in a number parameter and returns the largest digit
  * of the parameter.
  * 
+ * In number theory, Kaprekar's routine is an iterative algorithm that,
+ * with each iteration, takes a natural number in a given number base,
+ * creates two new numbers by sorting the digits of its number by descending
+ * and ascending order, and subtracts the second from the first to yield
+ * the natural number for the next iteration.
+ * 
  * @param {Number} num - A number parameter
  * @returns {Number} - Returns the largest digit from the parameter
  */
