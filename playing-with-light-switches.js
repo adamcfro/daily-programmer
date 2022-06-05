064e4c3f6dcd84e5ea4dd2684132d2614dc1f6b8
Adam Froehlich <adamcfro@gmail.com>
2022-06-05 10:09:59 -0400

    Solution for Daily Programmer problem, Plahing With Light Switches

---
+++ playing-with-light-switches.js
@@ -0,0 +1,33 @@
+/**
+ * This function takes in a number of lights and an array of ranges.
+ * All lights start in the off position, and are flipped every time a range
+ * passes over the switch. Returns the state of the switches after all ranges
+ * (inclusive) have been passed over.
+ * 
+ * @param {Number} numLights - The number of light switches
+ * @param  {...any/Array} args - A variable number of ranges in an array
+ * @returns {String} - Returns a string representation of the state of switches
+ */
+function lightSwitches (numLights, ...args) {
+  let lightStates = {};
+  for (let i = 0; i < numLights; i++) {
+    lightStates[i] = '.';
+  }
+  for (let arg of args) {
+    if (arg[0] <= arg[1]) {
+      for (let i = arg[0]; i <= arg[1]; i++) {
+        lightStates[i] = lightStates[i] === 'X' ? '.' : 'X'; 
+      }
+    } else {
+      for (let i = arg[0]; i >= arg[1]; i--) {
+        lightStates[i] = lightStates[i] === 'X' ? '.' : 'X';
+      }
+    }
+  }
+  let lightArr = Object.values(lightStates);
+  let finalState = '';
+  for (let i = 0; i < lightArr.length; i++) {
+    finalState += lightArr[i];
+  }
+  return finalState;
+}
