90b92a6ea2cf4b6b6df91b14528d0b1acf894810
Adam Froehlich <adamcfro@gmail.com>
2022-06-23 14:20:07 -0400

    Solution for Daily Programmer challenge, Nuts and Bolts

---
+++ nuts-and-bolts.js
@@ -0,0 +1,26 @@
+/**
+ * This function takes in a variable amount of space delimited value pairs,
+ * consisting of an item and the cost of the item. Tracks any changes in price
+ * for each item and returns a string representing those price changes.
+ * 
+ * @param  {String} args - A variable amount of space delimited value pairs
+ * @returns {String} - Returns a string of items with price changes
+ */
+function nutsAndBolts (...args) {
+  let inventory = {};
+  let priceChanges = [];
+  for (let i = 0; i < args.length; i++) {
+    let item = args[i].split(' ');
+    item[1] = Number(item[1]);
+    if (item[0] in inventory) {
+      if (inventory[item[0]] > item[1]) {
+        priceChanges.push(`${item[0]} -${inventory[item[0]] - item[1]}`);
+      } else if (inventory[item[0]] < item[1]) {
+        priceChanges.push(`${item[0]} +${item[1] - inventory[item[0]]}`);
+      }
+    } else {
+      inventory[item[0]] = (inventory[item[0]] || 0) + item[1];
+    }
+  }
+  return priceChanges.toString();
+}
